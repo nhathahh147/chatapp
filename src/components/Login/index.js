5466dd37c08a2f8d94528c8d11a8ec8a0b142b5f
nhathahh147 <nhathahh147@gmai.com>
2022-11-03 21:46:20 +0700

    login gg

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -7,10 +7,27 @@ const { Title } = Typography;
 
 const fbProvider = new firebase.auth.FacebookAuthProvider()
 
+const ggProvider = new firebase.auth.GoogleAuthProvider()
+
 export default function Login() {
     
     const handleFbLogin = async () => {
         const { additionalUserInfo, user } = await auth.signInWithPopup(fbProvider);
+        
+        if (additionalUserInfo?.isNewUser) {
+            addDocument('users', {
+                displayName: user.displayName,
+                email: user.email,
+                photoURL: user.photoURL,
+                uid: user.uid,
+                providerId: additionalUserInfo.providerId,
+                keywords: generateKeywords(user.displayName),
+            })
+        }
+    };
+
+    const handleGgLogin = async () => {
+        const { additionalUserInfo, user } = await auth.signInWithPopup(ggProvider);
         
         if (additionalUserInfo?.isNewUser) {
             addDocument('users', {
@@ -29,7 +46,10 @@ export default function Login() {
         <Row justify="center" style={{height: 800}}>
             <Col span={8}>
                 <Title style={{ textAlign: 'center' }} level={3}>Fun Chat</Title>
-                <Button style={{ width: '100%', marginBottom: 5}}>
+                <Button 
+                    style={{ width: '100%', marginBottom: 5}}
+                    onClick={handleGgLogin}
+                >
                     Đăng nhập bằng Google
                 </Button>
                 <Button 
